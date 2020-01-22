14a5afb89ebf02fd5156a9105d060e3723a5fa3e
mateo.areiza <mateo.areiza@globant.com>
2020-01-22 15:48:49 -0500

    Start with project develop

--- java/04-class-vs-instance/src/Main.java
+++ java/04-class-vs-instance/src/Main.java
@@ -3,6 +3,6 @@ import domain.Person;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person person = new Person(-1);
     }
 }
--- java/04-class-vs-instance/src/domain/Person.java
+++ java/04-class-vs-instance/src/domain/Person.java
@@ -1,4 +1,18 @@
 package domain;
 
 public class Person {
+    int age;
+
+    public Person(int age){
+        if (age>=0){
+            this.setAge(age);
+        } else {
+            System.out.println("Age is not valid, setting age to 0.");
+            this.setAge(0);
+        }
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
 }
