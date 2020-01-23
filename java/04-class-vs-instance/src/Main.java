cc657641d7423f59e3b158365292c69bcc93f428
areizas <areizas96@gmail.com>
2020-01-22 21:06:33 -0500

    Finish project

--- java/04-class-vs-instance/src/Main.java
+++ java/04-class-vs-instance/src/Main.java
@@ -3,7 +3,16 @@ import domain.Person;
 public class Main {
 
     public static void main(String[] args) {
-        Person person = new Person(10);
-        System.out.println(person.getAge());
+        int[] ages = new int[]{-1,10,16,18};
+
+        for (int i : ages){
+            Person person = new Person(i);
+            person.amIOld();
+            for (int j=0; j<3; j++){
+                person.yearPasses();
+            }
+            person.amIOld();
+            System.out.println();
+        }
     }
 }
--- java/04-class-vs-instance/src/domain/Person.java
+++ java/04-class-vs-instance/src/domain/Person.java
@@ -3,9 +3,9 @@ package domain;
 public class Person {
     int age;
 
-    public Person(int age){
-        if (age>=0){
-            this.setAge(age);
+    public Person(int initialAge){
+        if (initialAge>=0){
+            this.setAge(initialAge);
         } else {
             System.out.println("Age is not valid, setting age to 0.");
             this.setAge(0);
@@ -17,4 +17,18 @@ public class Person {
     }
 
     public int getAge() { return age;  }
+
+    public void yearPasses(){
+        this.age += 1;
+    }
+
+    public void amIOld(){
+        if(this.age<13){
+            System.out.println("You are young.");
+        } else if(this.age<18){
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+    }
 }
